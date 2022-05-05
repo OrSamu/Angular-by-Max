47f3e478b4449b567a99ec54be83bccea22491ed
Or Sam <orfacemailj@gmail.com>
2022-05-05 21:37:05 +0300

    recipe project in progress

--- recipes-shoppinglist-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipes-shoppinglist-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Hamburger Recipe', 'This is the first recipe',
+     'https://imageproxy.wolt.com/venue/5e6f93e9337c3135d34e1811/5f4f2ec2-682a-11ea-b01a-0a58646e9cf9_gdb8840.jpg'),
+     new Recipe('Hamburger Recipe', 'This is the first recipe',
+     'https://imageproxy.wolt.com/venue/5e6f93e9337c3135d34e1811/5f4f2ec2-682a-11ea-b01a-0a58646e9cf9_gdb8840.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
