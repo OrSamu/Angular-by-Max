10fe49b3e672ca36a4d3ab8715a449f26bb7bfee
Or Sam <orfacemailj@gmail.com>
2022-05-08 19:36:50 +0300

    keeping up with the project

--- recipes-shoppinglist-project/src/app/app.component.ts
+++ recipes-shoppinglist-project/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipes-shoppinglist-project';
+  showRecipesComponent: boolean = true;
+
+  onNavChange(event){
+    if (event === "Recipes"){
+      this.showRecipesComponent = true;
+      return;
+    }
+    this.showRecipesComponent = false;
+  }
 }
--- recipes-shoppinglist-project/src/app/header/header.component.ts
+++ recipes-shoppinglist-project/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, Output, EventEmitter } from '@angular/core'
 
 @Component({
     selector: 'app-header',
@@ -8,4 +8,10 @@ import { Component } from '@angular/core'
 
 export class HeaderComponent {
     collapsed: boolean = true;
+    @Output() navChanged = new EventEmitter<string>();
+
+    onNavClick(componentToShow: string){
+        console.log(componentToShow);
+        this.navChanged.emit(componentToShow);
+    }
 }
